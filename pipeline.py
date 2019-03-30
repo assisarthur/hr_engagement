ae97b41b372d8efe38ca5792a529fbde8ffd23d5
Arthur <arthur.a.s.2290@gmail.com>
2019-03-30 20:14:09 -0300

    Apresentação de uma forma de utilização do RH das informações obtidas

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -10,7 +10,7 @@ training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=0)
 
 # Average CV score on the training set was:1.0
-exported_pipeline = GaussianNB()
+exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=14, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
