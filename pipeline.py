f799a22dc5b650e31fef70ec916a879355b76e95
Arthur <arthur.a.s.2290@gmail.com>
2019-06-13 15:06:13 -0300

    Apresentação final v2. =)

--- pipeline.py
+++ pipeline.py
@@ -7,10 +7,10 @@ from sklearn.model_selection import train_test_split
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
 features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'].values, random_state=0)
+            train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:1.0
-exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.15000000000000002, min_samples_leaf=1, min_samples_split=14, n_estimators=100)
+# Score on the training set was:0.8974065909558773
+exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.8500000000000001, min_samples_leaf=16, min_samples_split=20, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
